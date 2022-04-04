5d27aef09405c8f7d911b5d9effe1b2e75f26110
Z Z <salamehziad95@gmail.com>
2022-04-04 10:04:30 -0400

    modelling sesnor noise with guassian distribution

--- simulations/SensorNoise/sensor_noise.py
+++ simulations/SensorNoise/sensor_noise.py
@@ -7,20 +7,17 @@ x_min = 0.0
 x_max = 2.0
 
 mean = 1.0 
-std1 = 0.11
 std2= 0.3
 std3= 0.15
 std4=0.1
 
 x = np.linspace(x_min, x_max, 100)
-y1 = scipy.stats.norm.pdf(x,mean,std1)
 y2 = scipy.stats.norm.pdf(x,mean,std2)
 y3 = scipy.stats.norm.pdf(x,mean,std3)
 y4 = scipy.stats.norm.pdf(x,mean,std4)
 
-plt.plot(x,y1, color='coral', label="Flex Resistor")
 plt.plot(x,y2, color='blue', label="MPU-6050")
-plt.plot(x,y3, color='green', label="PicoScope")
+plt.plot(x,y3, color='green', label="PicoScopePP877")
 plt.plot(x,y4, color='red', label="ADXL335")
 
 plt.legend(loc="upper left")
@@ -31,7 +28,7 @@ plt.xlim(x_min,x_max)
 
 plt.title('Plot a Normal Distribution of Sensor',fontsize=10)
 
-plt.xlabel('x')
+plt.xlabel('Accelerometer Position')
 plt.ylabel('Normal Distribution')
 
 plt.show()
