8ee6330d72beb7240363f2ee909075b731f724ab
aryastark-beep <58858681+aryastark-beep@users.noreply.github.com>
2022-07-18 21:26:26 -0400

    Minor modifications to server and client. Loopback msg adjustment.

--- network/client/client.js
+++ network/client/client.js
@@ -5,6 +5,14 @@ const serverAddress = "ws://127.0.0.1:5000";
 
 const ws = new WebSocket(serverAddress);
 
+//if not using localhost, uncomment below with wanted browser
+// const ws = new WebSocket(serverAddress, {
+//     headers: {
+//         "user-agent": "Mozilla"
+//     }
+// });
+
+
 ws.on('open', function() {
     ws.send("[CLIENT] Hello Server!");
 })
--- network/server-echo/server.js
+++ network/server-echo/server.js
@@ -15,11 +15,11 @@ wsServer.on('connection', function(socket) {
     console.log( (new Date()) + ": [INFO] Client connection established with IP: " + ip);
 
     socket.on('message', function(msg) {
-        console.log( (new Date()) + ": [INFO] Received message from client: " + msg);
-
+        console.log( (new Date()) + ": [INFO] Message received from client: " + msg);
+        
         //Broadcast to all connected clients
         wsServer.clients.forEach(function(client) {
-            client.send("[SERVER] The following message was received from a client: " + msg)
+            client.send(msg)
         });
     });
 });
