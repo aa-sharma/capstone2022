847e1301c0b09a3ce1942c395fed84b4c018d563
aryastark-beep <58858681+aryastark-beep@users.noreply.github.com>
2022-09-18 21:24:07 -0400

    Firebase authentication user signup

--- website/auth.js
+++ website/auth.js
@@ -1,97 +1,76 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import {
-  getAuth,
-  onAuthStateChanged,
-} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-analytics.js";
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+    import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+    import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
 
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDPYvTnU3I0P_IdKOSnmrmGwldvhUZjJoI",
-    authDomain: "capstone2022-4f612.firebaseapp.com",
-    projectId: "capstone2022-4f612",
-    storageBucket: "capstone2022-4f612.appspot.com",
-    messagingSenderId: "904570161227",
-    appId: "1:904570161227:web:506cd785ef1c8c6efee5ce",
-    measurementId: "G-WV599N592B"
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const analytics = getAnalytics(app);
-
-
-document.getElementById('signup-btn').addEventListener('click', signup);
-
-
-const auth = getAuth(app);
-// onAuthStateChanged(auth, (user) => {
-//   if (user) {
-//     // User is signed in, see docs for a list of available properties
-//     // https://firebase.google.com/docs/reference/js/firebase.User
-//     const uid = user.uid;
-
-//     // document.getElementById("user_div").style.display = "initial";
-//     // document.getElementById("login_div").style.display = "none";
-
-//   } else {
-//     // User is signed out
-//     // document.getElementById("user_div").style.display = "none";
-//     // document.getElementById("login_div").style.display = "initial";
-// }
-// });
-
-function signup() {
-    console.log("[INFO] Attempting to register user...")
-    var userEmail = document.getElementById("email_field").value;
-    var userPassword = document.getElementById("password_field").value;
-    var firstName = document.getElementById("first_name").value;
-    var lastName = document.getElementById("last_name").value;
-
+    // import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-analytics.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+  
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyDPYvTnU3I0P_IdKOSnmrmGwldvhUZjJoI",
+      authDomain: "capstone2022-4f612.firebaseapp.com",
+      databaseURL: "https://capstone2022-4f612-default-rtdb.firebaseio.com",
+      projectId: "capstone2022-4f612",
+      storageBucket: "capstone2022-4f612.appspot.com",
+      messagingSenderId: "904570161227",
+      appId: "1:904570161227:web:506cd785ef1c8c6efee5ce",
+      measurementId: "G-WV599N592B"
+    };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    // const analytics = getAnalytics(app);
     const auth = getAuth(app);
-    createUserWithEmailAndPassword(auth, userEmail, userPassword)
-    .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        window.alert("user created for :" + firstName + lastName)
-        // ...
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+    const database = getDatabase(app);
 
-        window.alert("Error: " + errorMessage);
+    signUp.addEventListener('click',(e) => {
+        var email = document.getElementById('email_field').value;
+        var password = document.getElementById('password_field').value;
+        var first_name = document.getElementById('first_name').value;
+        var last_name = document.getElementById('last_name').value;
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            set(ref(database, 'users/' + user.uid), {
+                email: email,
+                first_name: first_name,
+                last_name: last_name
+            })
+                .then( () => {
+                    alert('User created succesfully');
+                })
+                .catch((error) => {
+                    alert(error);
+                });
+            
+            // ...
+            })
+            .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+            // ..
+            });
     });
 
-}
 
-function login() {
-    console.log("[INFO] Attempting to login user...")
-    var userEmail = document.getElementById("email_field").value;
-    var userPassword = document.getElementById("password_field").value;
+    login.addEventListener('click', (e) => {
+        var email = document.getElementById('email_field').value;
+        var password = document.getElementById('password_field').value;
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+        })
+            .then( () => {
+                alert('Login successful');
+            })
+            .catch((error) => {
+                alert(error);
+            }); 
 
-    const auth = getAuth();
-    signInWithEmailAndPassword(auth, userEmail, userPassword)
-    .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        // ...
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-
-        window.alert("Error: " + errorMessage);
     });
 
-}
-
-function logout() {
-    const auth = getAuth();
-    signOut(auth).then(() => {
-      // Sign-out successful.
-    }).catch((error) => {
-      // An error happened.
-    });
-}
