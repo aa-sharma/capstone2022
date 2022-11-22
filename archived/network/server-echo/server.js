2e13cc1cb09d3a7fec669b756b459e6090d4e5e5
Aashna Sharma <aashna@Aashnas-MacBook-Pro.local>
2022-11-22 01:17:31 -0500

    Client UUIDs setup from js ws server

--- archived/network/server-echo/server.js
+++ archived/network/server-echo/server.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 const { v4: uuidv4 } = require("uuid");
 const WeakMap = require("weak-map");
+const { uuid } = require('uuidv4');
 
 const PORT = 5050;
 const socketMap = new WeakMap();
@@ -9,26 +10,32 @@ const socketMap = new WeakMap();
 const wsServer = new WebSocket.Server({ port: PORT });
 console.log( (new Date()) + ": [INFO] Starting WebSocket server on port " + PORT);
 
+const client_uuids = {}
 
 // callback function upon establishing connection
 wsServer.on('connection', function(socket, req) {
     // assign uuid to connecting client
     socket.uuid = uuidv4();
-    socketMap.set(socket, socket.uuid);
 
     const ip = req.socket.remoteAddress
     console.log( (new Date()) + ": [INFO] Client connection established with IP: " + ip);
+    socketMap.set(socket, socket.uuid);
+
+    var i=0
+    wsServer.clients.forEach(function(client) {
+        console.log((new Date()) + ": [INFO] Client " + i + " | UUID : " + client.uuid)
+        client_uuids.i = client.uuid
+        i=i+1
+    }) ;
+    console.log(client_uuids)
 
     socket.on('message', function(msg) {
-        console.log( (new Date()) + ": [INFO] Message received from client: " + msg);
-        console.log((new Date()) + ": [INFO] Session id: " + socket.uuid);
+        console.log((new Date()) + ": [INFO] Message received from Client ID: " + socket.uuid);
+        console.log( (new Date()) + ": [INFO] Message:\n\n" + msg + "\n\n");
         //Broadcast to all connected clients
         wsServer.clients.forEach(function(client) {
             client.send(msg)
         });
-        // TODO : Get the client message, check if it for broadcast or unicast, check client ID and send
-        // the message to only that client. This means all packages coming to the server must have the 
-        // first value as either 'unicast' or 'multicast'
     });
 });
 
