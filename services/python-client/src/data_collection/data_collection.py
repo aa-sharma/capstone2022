20858599af1e1ab1c70dd9277e29f036d0fe1118
Aashna Sharma <aashna@Aashnas-MacBook-Pro.local>
2022-11-26 23:47:15 -0500

    Reformat XYZDict structure

--- services/python-client/src/data_collection/data_collection.py
+++ services/python-client/src/data_collection/data_collection.py
@@ -90,78 +90,82 @@ def repackageCartesian(XYZ):
     |  Key     |      Point Map       |
     +----------+----------------------+
     |    0     |        palm A       |
-    |    1     |        palm B       |
-    |    2     |        palm C       |
-    |    3     |        palm D       |
-    |    4     |        index A      |
-    |    5     |        index B      |
-    |    6     |        index C      |
-    |    7     |        index D      |
-    |    8     |        middle A     |
-    |    9     |        middle B     |
-    |    10    |        middle C     |
-    |    11    |        middle D     |
-    |    12    |        ring A       |
-    |    13    |        ring B       |
-    |    14    |        ring C       |
-    |    15    |        ring D       |
-    |    16    |        pinky A      |
-    |    17    |        pinky B      |
-    |    18    |        pinky C      |
-    |    19    |        pinky D      |
-    |    20    |        thumb A      |
-    |    21    |        thumb B      |
-    |    22    |        thumb C      |
-    |    23    |        thumb D      |
+    |    1     |        index A      |
+    |    2     |        index B      |
+    |    3     |        index C      |
+    |    4     |        index D      |
+    |    5     |        middle A     |
+    |    6     |        middle B     |
+    |    7     |        middle C     |
+    |    9     |        middle D     |
+    |    9     |        ring A       |
+    |    10    |        ring B       |
+    |    11    |        ring C       |
+    |    12    |        ring D       |
+    |    13    |        pinky A      |
+    |    14    |        pinky B      |
+    |    15    |        pinky C      |
+    |    16    |        pinky D      |
+    |    17    |        thumb A      |
+    |    18    |        thumb B      |
+    |    19    |        thumb C      |
+    |    20    |        thumb D      |
     +----------+----------------------+
     """
     # Inital
     XYZDict = {
-        # "palmA": {"x": "7", "y": "0", "z": "3"},
-        # "indexA": {"x": "3", "y": "2", "z": "9"},
-        # "indexB": {"x": "8", "y": "8", "z": "8"},
-        0: {"x": "8", "y": "8", "z": "8"},
-        1: {"x": "8", "y": "8", "z": "8"},
-        2: {"x": "8", "y": "8", "z": "8"},
-        3: {"x": "8", "y": "8", "z": "8"},
-        3: {"x": "1", "y": "6", "z": "1"},
-        4: {"x": "1", "y": "6", "z": "1"},
-        5: {"x": "1", "y": "6", "z": "1"},
-        6: {"x": "7", "y": "0", "z": "3"},
-        7: {"x": "3", "y": "2", "z": "9"},
-        8: {"x": "8", "y": "8", "z": "8"},
-        9: {"x": "1", "y": "6", "z": "1"},
-        10: {"x": "1", "y": "6", "z": "1"},
-        11: {"x": "1", "y": "6", "z": "1"},
-        12: {"x": "8", "y": "8", "z": "8"},
-        13: {"x": "1", "y": "6", "z": "1"},
-        14: {"x": "1", "y": "6", "z": "1"},
-        15: {"x": "1", "y": "6", "z": "1"},
-        16: {"x": "7", "y": "0", "z": "3"},
-        17: {"x": "3", "y": "2", "z": "9"},
-        18: {"x": "8", "y": "8", "z": "8"},
-        19: {"x": "1", "y": "6", "z": "1"},
-        20: {"x": "1", "y": "6", "z": "1"},
-        21: {"x": "1", "y": "6", "z": "1"},
-        22: {"x": "1", "y": "6", "z": "1"},
-        23: {"x": "1", "y": "6", "z": "1"}
+        "palmA": {"x": "7", "y": "0", "z": "3"},
+        "indexA": {"x": "3", "y": "2", "z": "9"},
+        "indexB": {"x": "8", "y": "8", "z": "8"},
+        "indexC": {"x": "8", "y": "8", "z": "8"},
+        "indexD": {"x": "1", "y": "6", "z": "1"},
+        "middleA": {"x": "7", "y": "0", "z": "3"},
+        "middleB": {"x": "3", "y": "2", "z": "9"},
+        "middleC": {"x": "8", "y": "8", "z": "8"},
+        "middleD": {"x": "1", "y": "6", "z": "1"},
+        "ringA": {"x": "1", "y": "6", "z": "1"},
+        "ringB": {"x": "1", "y": "6", "z": "1"},
+        "ringC": {"x": "8", "y": "8", "z": "8"},
+        "ringD": {"x": "1", "y": "6", "z": "1"},
+        "pinkyA": {"x": "1", "y": "6", "z": "1"},
+        "pinkyB": {"x": "1", "y": "6", "z": "1"},
+        "pinkyC": {"x": "7", "y": "0", "z": "3"},
+        "pinkyD": {"x": "3", "y": "2", "z": "9"},
+        "thumbA": {"x": "8", "y": "8", "z": "8"},
+        "thumbB": {"x": "1", "y": "6", "z": "1"},
+        "thumbC": {"x": "1", "y": "6", "z": "1"},
+        "thumbD": {"x": "1", "y": "6", "z": "1"},
     }
 
-    # mapping = {
-    #     1: "palmA",
-    #     2: "indexA"
-    # }
+    mapping = {
+        0: "palmA",
+        1: "indexA",
+        2: "indexB",
+        3: "indexC",
+        4: "indexD",
+        5: "middleA",
+        6: "middleB",
+        7: "middleC",
+        8: "middleD",
+        9: "ringA",
+        10: "ringB",
+        11: "ringC",
+        12: "ringD",
+        13: "pinkyA",
+        14: "pinkyB",
+        15: "pinkyC",
+        16: "pinkyD",
+        17: "thumbA",
+        18: "thumbB",
+        19: "thumbC",
+        20: "thumbD"
+    }
 
     ############ Unpack Array ############
 
     X = XYZ[0]
     Y = XYZ[1]
     Z = XYZ[2]
-
-    # [TODO] Palm (RPY) [PARKED] HW IMPLEMENTATION IN PROGRESS
-    X[0] = [0, 0, 0, 0]    # temporarily
-    Y[0] = [0, 0, 0, 0]    # temporarily
-    Z[0] = [0, 0, 0, 0]    # temporarily
 
     # j
     # 0 --> Palm
@@ -179,13 +183,15 @@ def repackageCartesian(XYZ):
     k = 0
     for j in range(6):
         for i in range(4):
-            # XYZDict[mapping[k]]["x"] = X[j][i]
-            # XYZDict[mapping[k]]["y"] = Y[j][i]
-            # XYZDict[mapping[k]]["z"] = Z[j][i]
-            XYZDict[k]["x"] = X[j][i]
-            XYZDict[k]["y"] = Y[j][i]
-            XYZDict[k]["z"] = Z[j][i]
-
+            if j == 0:
+                XYZDict[mapping[k]]["x"] = X[j]
+                XYZDict[mapping[k]]["y"] = Y[j]
+                XYZDict[mapping[k]]["z"] = Z[j]
+                break
+            else:
+                XYZDict[mapping[k]]["x"] = X[j][i]
+                XYZDict[mapping[k]]["y"] = Y[j][i]
+                XYZDict[mapping[k]]["z"] = Z[j][i]
             k = k+1
 
     logger.debug(XYZDict)
