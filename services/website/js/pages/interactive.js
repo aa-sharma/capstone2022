fff17d5a596e7f2178fdb55db46af63c74dc988f
lucasrahn09 <lucasrahn09@gmail.com>
2022-11-27 00:20:36 -0500

    move around stuff into interactive.js

---
+++ services/website/js/pages/interactive.js
@@ -0,0 +1,52 @@
+import { SERVER_BASE_URL } from "../modules/constants.js";
+import API from "../modules/api.js";
+import Render from "../modules/render/render.js";
+
+const token = localStorage["token"];
+
+const socket = io(SERVER_BASE_URL, {
+  auth: { "x-auth-token": token },
+  transports: ["websocket"],
+});
+
+const api = new API({ url: "/api/exercise", token });
+const { json } = await api.call();
+const render = new Render();
+render.animate();
+
+const startLevelButton = document.getElementById("start-level");
+const stopLevelButton = document.getElementById("stop-level");
+
+socket.on("connect", () => {
+  console.log(`Successfully connected websocket server: ${SERVER_BASE_URL}`);
+});
+
+socket.on("connect_error", (data) => {
+  console.error(
+    `Websocket connection to ${SERVER_BASE_URL} failed, with reason: ${data}`
+  );
+});
+
+socket.on("disconnect", () => {
+  console.log(`Disconnected from websocket server: ${SERVER_BASE_URL}`);
+});
+
+socket.on("python_client_connected", (data) => {
+  console.log(data);
+});
+
+socket.on("python_client_data", (xyzDict) => {
+  for (key in xyzDict) {
+    render.move(key, xyzDict[key]);
+  }
+});
+
+startLevelButton.addEventListener("click", (ev) => {
+  console.log("start level");
+  socket.emit("user_start_exercise", { exercise: json.items[0] });
+});
+
+stopLevelButton.addEventListener("click", (ev) => {
+  console.log("stop level");
+  socket.emit("user_stop_exercise");
+});
